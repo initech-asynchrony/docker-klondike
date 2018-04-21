c3108a8c10b6b5b48913c6c4497acad466dc21c5
Bruce Harrison <leeharrison1984@gmail.com>
2018-04-21 14:32:19 -0500

    Increase sync interval to 5 minutes

--- run-klondike.sh
+++ run-klondike.sh
@@ -9,7 +9,7 @@ if [ -n "$S3_BUCKET" ]; then
   /usr/bin/aws s3 sync s3://$S3_BUCKET /klondike/data
 
   echo "Creating sync cron job..."
-  echo "*/1 * * * * /usr/bin/s3-sync.sh ${S3_BUCKET} >> /var/log/s3-sync.log 2>&1" | /usr/bin/crontab -
+  echo "*/5 * * * * /usr/bin/s3-sync.sh ${S3_BUCKET} >> /var/log/s3-sync.log 2>&1" | /usr/bin/crontab -
   echo "Ensure Cron is running in background"
   /usr/sbin/crond
 fi
